bcfb23a47a69c01fe74a63942313ac0a422ae705
leventekobor94 <levente.kobor94@gmail.com>
2019-10-03 23:10:23 +0200

    We should delete this crap before shipping.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from mosaic import create_mosaic
 
@@ -14,9 +14,9 @@ def upload():
     if request.method == 'POST' and 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         full_filename = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename)
-        return  render_template('image.html', filename=create_mosaic(full_filename))
+        return  render_template("image.html", filename=create_mosaic(full_filename))
     return render_template('index.html')
-
+    
 
 if __name__ == '__main__':
 	app.run(debug=True)
